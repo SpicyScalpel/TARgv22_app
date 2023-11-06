4119d5cd5ef7efa27d3e7e1e261cc350abe0ba08
opilane <opilane@HP-CZC2349HTP>
2023-11-06 10:53:19 +0200

    Add project files.

---
+++ TARgv22_app/StartPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TARgv22_app
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class StartPage : ContentPage
+    {
+        public StartPage()
+        {
+            Button Ent_btn = new Button
+            {
+                Text= "Entry page",
+                BackgroundColor= Color.LightSkyBlue,
+            };
+
+            Button Time_btn = new Button
+            {
+                Text = "Timer page",
+                BackgroundColor = Color.LightSkyBlue,
+            };
+
+            StackLayout st= new StackLayout 
+            { 
+               Children= {Ent_btn, Time_btn}, //vse elementi, kotorije hotim videtj na stranice
+               BackgroundColor= Color.DarkBlue,
+            };
+
+            Content= st;
+            Ent_btn.Clicked += Ent_btn_Clicked;
+            Time_btn.Clicked += Time_btn_Clicked;
+        }
+
+        private async void Time_btn_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new TimerPage());
+        }
+
+        private async void Ent_btn_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new EntryPage());
+        }
+    }
+}
