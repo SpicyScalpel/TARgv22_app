f189e5a989f817c05ebbd87d3438986cbca53943
Karina <karina.kulakova.97@gmail.com>
2023-11-13 11:21:41 +0200

    New StartPage with new functions

---
+++ TARgv22_app/DateTimePage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TARgv22_app
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DateTimePage : ContentPage
+    {
+        Label lb;
+        DatePicker datePicker;
+        TimePicker timePicker;
+        public DateTimePage()
+        {
+            lb = new Label
+            {
+                Text = "Vali mingi kuupäev",
+                BackgroundColor = Color.BurlyWood,
+            };
+            datePicker = new DatePicker
+            {
+                Format = "D",
+                MinimumDate = DateTime.Now.AddDays(-10),
+                MaximumDate= DateTime.Now.AddDays(10),
+                TextColor= Color.Black,
+            };
+            datePicker.DateSelected += DatePicker_DateSelected;
+            timePicker = new TimePicker
+            {
+                Time = new TimeSpan(12, 0, 0),
+            };
+
+            timePicker.PropertyChanged += TimePicker_PropertyChanged;
+            AbsoluteLayout abs = new AbsoluteLayout { Children= { lb,datePicker,timePicker } };
+            AbsoluteLayout.SetLayoutBounds(lb, new Rectangle(0.1, 0.2, 200, 100));
+            AbsoluteLayout.SetLayoutFlags(lb, AbsoluteLayoutFlags.PositionProportional);
+            AbsoluteLayout.SetLayoutBounds(datePicker, new Rectangle(0.1, 0.5, 300, 100));
+            AbsoluteLayout.SetLayoutFlags(datePicker, AbsoluteLayoutFlags.PositionProportional);
+            AbsoluteLayout.SetLayoutBounds(timePicker, new Rectangle(0.5, 0.5, 400, 100));
+            AbsoluteLayout.SetLayoutFlags(timePicker, AbsoluteLayoutFlags.PositionProportional);
+            Content = abs;
+        }
+
+        private void TimePicker_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            lb.Text = timePicker.Time.ToString();
+        }
+
+        private void DatePicker_DateSelected(object sender, DateChangedEventArgs e)
+        {
+            lb.Text = e.NewDate.ToString("G");
+        }
+    }
+}
---
+++ TARgv22_app/StartPage1.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TARgv22_app
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class StartPage1 : ContentPage
+    {
+        List<ContentPage> pages = new List<ContentPage>() { new EntryPage(), new BoxView_Page(), new TimerPage(), new DateTimePage(), new StepperSliderPage()};
+        List<string> teksts = new List<string> { "Ava Entry leht", "Ava BoxView leht", "Ava Timer leht", "Ava DateTime leht" };
+        StackLayout st;
+        public StartPage1()
+        {
+            st = new StackLayout
+            {
+                Orientation = StackOrientation.Vertical,
+                BackgroundColor = Color.Green,
+            };
+            for (int i = 0; i< pages.Count; i++) 
+            {
+                Button button = new Button
+                {
+                    Text = teksts[i],
+                    TabIndex = i,
+                    BackgroundColor = Color.Red,
+                    TextColor = Color.Green,
+                };
+                st.Children.Add(button);
+                button.Clicked += Button_Clicked;
+            }
+            ScrollView sv = new ScrollView { Content = st };
+            Content = sv;
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            Button btn= (Button)sender;
+            await Navigation.PushAsync(pages[btn.TabIndex]);
+        }
+    }
+}
